4e2210246ee282d50b1327222e4d0c073da3c8a4
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-06-22 11:38:01 -0400

    Added event listener for submit button

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let bookshelf = [];
 
 const ui =   document.querySelector(".popup");
 function popup(){
-   
-  ui.style.display = "none";
+  ui.style.display = "flex"; 
 }
+
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  const formData = new FormData(e.target);
+ alert(formData.get("title"));
+});
+
+
+
+
