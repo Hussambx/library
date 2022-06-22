23c339dadf7f1ebece1a890a6ff23230bc4056e1
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-06-22 12:00:06 -0400

    Form data now is stored into an array

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let bookshelf = [];
-
 const ui =   document.querySelector(".popup");
 function popup(){
   ui.style.display = "flex"; 
@@ -8,9 +7,38 @@ function popup(){
 
 document.querySelector('form').addEventListener('submit', (e) => {
   const formData = new FormData(e.target);
- alert(formData.get("title"));
+ 
+
+  let bookx = new book(formData.get('title'),formData.get('title'),formData.get('pages'));
+  bookshelf.push(bookx);
+create(bookx)
+alert(bookx.title);
 });
 
 
+function create(book){
 
 
+
+
+
+
+}
+
+
+
+
+
+
+
+function book(title,author,pages){
+
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  
+
+  this.saytitle= function(){
+    return this.title;
+  }
+  }
