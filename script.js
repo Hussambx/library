e7f8e3b1d6639f4aca564af87e89198fd082c4b8
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-06-23 10:20:55 -0400

    create() creates new interface for the book

--- script.js
+++ script.js
@@ -19,7 +19,26 @@ alert(bookx.title);
 
 
 function create(book){
-  
+  let div = document.createElement("div");
+  let titlex = document.createElement("h3");
+  let authorx = document.createElement("h3");
+  let pagesx = document.createElement("h3");
+  let read  = document.createElement("button");
+  let remove = document.createElement("button");
+
+titlex.innerText = book.title;
+authorx.innerText = book.author;
+pagesx.innerText = book.pages;
+read.innerText = "Read";
+remove.innerText = "Remove";
+remove.id = book.title;
+document.getElementById('main').appendChild(div);
+
+div.appendChild(titlex);
+div.appendChild(authorx);
+div.appendChild(pagesx);
+div.appendChild(read);
+div.appendChild(remove);
 
 }
 
