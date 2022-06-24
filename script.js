7df1792ba4d8e99e2fb56b82c3a0df89d3a5a924
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-06-24 11:11:14 -0400

    Finished remove function +added div to the array

--- script.js
+++ script.js
@@ -10,59 +10,61 @@ document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault() 
   ui.style.display = "none";
   ui.reset();
-  let bookx = new book(formData.get('title'),formData.get('title'),formData.get('pages'));
+  var div = document.createElement("div");
+ 
+  var bookx = new book(formData.get('title'),formData.get('title'),formData.get('pages'),div);
   bookshelf.push(bookx);
-create(bookx)
-alert(bookx.title);
+create(bookx,div)
 });
 
 
-function create(book){
-  let div = document.createElement("div");
+function create(book,div){
+  
   let titlex = document.createElement("h3");
   let authorx = document.createElement("h3");
   let pagesx = document.createElement("h3");
   let read  = document.createElement("button");
-  let remove = document.createElement("button");
-
+  let removea = document.createElement("button");
 titlex.innerText = book.title;
 authorx.innerText = book.author;
 pagesx.innerText = book.pages;
+div.class = book.title;
 read.innerText = "Read";
-remove.innerText = "Remove";
-remove.id = book.title;
-remove.class = "removal";
+removea.innerText = "Remove";
+removea.id = book.title;
+removea.class = "removal";
 document.getElementById('main').appendChild(div);
-
 div.appendChild(titlex);
 div.appendChild(authorx);
 div.appendChild(pagesx);
 div.appendChild(read);
-div.appendChild(remove);
-removex(remove);
+div.appendChild(removea);
+const man = div.parentNode;
+removex(removea,div);
 }
 
 
-function removex(tar){
+function removex(tar,diva){
 tar.addEventListener("mousedown",function(e){
-for(const books of bookshelf){
-  if(books.title==e.target.id){
-   bookshelf.splice(books);
-   alert(bookshelf.length);
+  console.log("button pressed: "+e.target.id)
+  console.log("length of array"+bookshelf.length);
+  for(let i =0; i<bookshelf.length; i++){
+    if(bookshelf[i].title==e.target.id){
+     console.log("found");
+     console.log("location of #"+i);
+   document.getElementById('main').removeChild(bookshelf[i].div);
+   bookshelf.splice(i,1);
+    }
   }
-}
 });
 }
 
-
-
-
-function book(title,author,pages){
+function book(title,author,pages,div){
 
   this.title = title;
   this.author = author;
   this.pages = pages;
-  
+  this.div = div;
 
   this.saytitle= function(){
     return this.title;
